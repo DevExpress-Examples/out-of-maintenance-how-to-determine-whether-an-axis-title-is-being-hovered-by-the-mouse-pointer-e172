d9992d5b43df0c4e11a4667bed1cf020494a150b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/HitTestingAxisTitle/Form1.Designer.vb
@@ -0,0 +1,98 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace HitTestingAxisTitle
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+            Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim Series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim SideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim SeriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("A", New Object() {CType(12.0R, Object)})
+            Dim SeriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("B", New Object() {CType(7.0R, Object)})
+            Dim SeriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("C", New Object() {CType(9.0R, Object)})
+            Dim SeriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("D", New Object() {CType(5.0R, Object)})
+            Dim SideBySideBarSeriesLabel2 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim ChartTitle1 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Series1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'chartControl1
+            '
+            XyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = True
+            XyDiagram1.AxisX.Range.SideMarginsEnabled = True
+            XyDiagram1.AxisX.Title.Visible = True
+            XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            XyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
+            XyDiagram1.AxisY.Range.SideMarginsEnabled = True
+            XyDiagram1.AxisY.Title.Visible = True
+            XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = XyDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Visible = False
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            SideBySideBarSeriesLabel1.LineVisible = True
+            Series1.Label = SideBySideBarSeriesLabel1
+            Series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
+            Series1.Name = "Series 1"
+            Series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {SeriesPoint1, SeriesPoint2, SeriesPoint3, SeriesPoint4})
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {Series1}
+            SideBySideBarSeriesLabel2.LineVisible = True
+            Me.chartControl1.SeriesTemplate.Label = SideBySideBarSeriesLabel2
+            Me.chartControl1.Size = New System.Drawing.Size(553, 317)
+            Me.chartControl1.TabIndex = 0
+            ChartTitle1.Font = New System.Drawing.Font("Tahoma", 14.0!)
+            ChartTitle1.Text = "Hover the mouse pointer over an AxisTitle or AxisLabelItem"
+            ChartTitle1.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+            ChartTitle1.WordWrap = True
+            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {ChartTitle1})
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(553, 317)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Series1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+		#End Region
+
+		Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+	End Class
+End Namespace
+
