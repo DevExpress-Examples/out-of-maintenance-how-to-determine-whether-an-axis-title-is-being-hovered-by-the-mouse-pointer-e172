ab3e8f2228028d4993c4e95b6663c7e81c069c1d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to determine whether an axis title is being hovered by the mouse pointer
+
+
+<p>This example demonstrates how you can determine whether an axis title or axis label item is being hovered by the mouse pointer, and if so, display its type (AxisTitleX or AxisTitleY) or axis value in a message box.</p>
+
+<br/>
+
+
---
+++ VB/HitTestingAxisTitle/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace HitTestingAxisTitle
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs) Handles chartControl1.ObjectHotTracked
+            If TypeOf e.AdditionalObject Is AxisTitle Then
+                MessageBox.Show(e.AdditionalObject.GetType().ToString())
+            End If
+            If TypeOf e.AdditionalObject Is AxisLabelItem Then
+                MessageBox.Show(CType(e.AdditionalObject, AxisLabelItem).AxisValue.ToString())
+            End If
+        End Sub
+    End Class
+End Namespace
